de08530f7bdf8baea5d19c9e19543edc31c8d3be
JLUVicent <17390955615@163.com>
2022-10-24 23:51:08 +0800

    2022-10-24

---
+++ 0014.最长公共前缀/0014-最长公共前缀.py
@@ -0,0 +1,29 @@
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # ×İÏòÉ¨Ãè
+        # if not strs:
+        #     return ""
+        # length, count = len(strs[0]), len(strs)
+        # for i in range(length):
+        #     c = strs[0][i]
+        #     if any(i == len(strs[j]) or strs[j][i] != c for j in range(1, count)):
+        #         return strs[0][:i]
+        # return strs[0]
+        # ºáÏòÉ¨Ãè
+        if not strs:
+            return ""
+        prefix, count = strs[0], len(strs)
+        for i in range(1, count):
+            prefix = self.lcp(prefix, strs[i])
+            if not prefix:
+                break
+        return prefix
+
+    def lcp(self, str1, str2):
+        length, index = min(len(str1), len(str2)), 0
+        while index < length and str1[index] == str2[index]:
+            index += 1
+        return str1[:index]
+
