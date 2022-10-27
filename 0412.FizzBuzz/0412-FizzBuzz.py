27c38b0919396870cc7123326ae7d33e2ad2d56c
JLUVicent <17390955615@163.com>
2022-10-27 23:51:11 +0800

    2022-10-27

---
+++ 0412.FizzBuzz/0412-FizzBuzz.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = [0]*n
+        for i in range(n):
+            t = i+1
+            if t % 3 == 0 and t % 5 == 0:
+                answer[i] = "FizzBuzz"
+            elif t % 3 == 0 and t % 5 != 0:
+                answer[i] = "Fizz"
+            elif t % 3 != 0 and t % 5 == 0:
+                answer[i] = "Buzz"
+            else:
+                answer[i] = str(t)
+        return answer
---
+++ 0506.相对名次/0506-相对名次.py
@@ -0,0 +1,27 @@
+
+
+class Solution:
+    desc = ("Gold Medal", "Silver Medal", "Bronze Medal")
+
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        ans = [""]*len(score)
+        arr = sorted(enumerate(score), key=lambda x: -x[1])
+        for i, (idx, _) in enumerate(arr):
+            ans[idx] = self.desc[i] if i < 3 else str(i+1)
+        return ans
+        # result = [0]*len(score)
+        # sort_score = sorted(score, reverse=True)
+        # hash_map = {}
+        # for i, value in enumerate(sort_score):
+        #     hash_map[value] = i+1
+        # for j in range(len(score)):
+        #     if hash_map[score[j]] == 1:
+        #         result[j] = "Gold Medal"
+        #     elif hash_map[score[j]] == 2:
+        #         result[j] = "Silver Medal"
+        #     elif hash_map[score[j]] == 3:
+        #         result[j] = "Bronze Medal"
+        #     else:
+        #         result[j] = str(hash_map[score[j]])
+        # return result
+
