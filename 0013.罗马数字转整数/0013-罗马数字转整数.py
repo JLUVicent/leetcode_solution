1c74127b23dd25e7408049ec5e5a1d4f40d63768
JLUVicent <17390955615@163.com>
2022-11-07 23:51:11 +0800

    2022-11-07

---
+++ 0013.罗马数字转整数/0013-罗马数字转整数.py
@@ -0,0 +1,21 @@
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        result = 0
+        s_value = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        for i in range(len(s)):
+            if i < len(s)-1 and s_value[s[i]] < s_value[s[i+1]]:
+                result -= s_value[s[i]]
+            else:
+                result += s_value[s[i]]
+        return result
+
---
+++ 0165.比较版本号/0165-比较版本号.py
@@ -0,0 +1,20 @@
+from itertools import zip_longest
+from textwrap import fill
+
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        # v1, v2 = version1.split("."), version2.split(".")
+        # if len(v1) > len(v2):
+        #     for i in range(len(v1)-len(v2)):
+        #         v2.append('0')
+        # else:
+        #     for i in range(len(v2)-len(v1)):
+        #         v1.append('0')
+
+        # for i in range(len(v1)):
+        for v1, v2 in zip_longest(version1.split('.'), version2.split('.'), fillvalue=0):
+            x, y = int(v1), int(v2)
+            if x != y:
+                return 1 if x > y else -1
+        return 0
