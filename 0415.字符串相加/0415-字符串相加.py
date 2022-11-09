4e58203df7730e6306667fb0c34937632e75e62a
JLUVicent <17390955615@163.com>
2022-11-09 23:51:09 +0800

    2022-11-09

---
+++ 0415.字符串相加/0415-字符串相加.py
@@ -0,0 +1,18 @@
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        num1, num2 = num1[::-1], num2[::-1]
+        n, carry = max(len(num1), len(num2)), 0
+        ans = ''
+
+        for i in range(n):
+            a = int(num1[i]) if i < len(num1) else 0
+            b = int(num2[i]) if i < len(num2) else 0
+            sum_ = (a+b+carry) % 10
+            ans += str(sum_)
+            carry = (a+b+carry)//10
+        if carry:
+            ans += '1'
+        return ans[::-1]
+
