027831e7b49d1211db79896d5dd17cdaaa8c0cd4
JLUVicent <17390955615@163.com>
2023-04-11 23:51:12 +0800

    2023-04-11

--- 0115.不同的子序列/0115-不同的子序列.cpp
+++ 0115.不同的子序列/0115-不同的子序列.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         vector<vector<uint64_t>> dp(s.size()+1,vector<uint64_t>(t.size()+1,0));
-        for( int i = 0; i < s.size()+1; i++){
+        for( int i = 0; i <= s.size(); i++){
             dp[i][0] = 1;
         }
-        for( int i = 1; i <= s.size(); i++){
+        for( int  i = 1; i <= s.size(); i++){
             for( int j = 1; j <= t.size(); j++){
                 if( s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];//如果不相等取前一个的状态
                 }
             }
         }
         return dp[s.size()][t.size()];
+
     }
 };
--- 0392.判断子序列/0392-判断子序列.cpp
+++ 0392.判断子序列/0392-判断子序列.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        // 暴力模拟
-        // int tmp = -1;
-        // int count = 0;
-        // for (int i = 0; i < s.size(); i++) {
-        //     for (int j = 0; j < t.size(); j++) {
-        //         if (s[i] == t[j] && j > tmp) {
-        //             tmp = j;
-        //             count++;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return (count == s.size());
-
-        // 动态规划
         vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,0));
-        for( int i = 1; i <=  s.size(); i++){
+        for( int i = 1; i <= s.size(); i++){
             for( int j = 1; j <= t.size(); j++){
                 if( s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    dp[i][j] = dp[i-1][j-1]+1;
                 }else{
                     dp[i][j] = max( dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         return (dp[s.size()][t.size()] == s.size());
+
     }
 };
