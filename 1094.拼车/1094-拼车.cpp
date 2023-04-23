1ff064fb1f49052a71abdcd9ca5f40b9c9e3e618
JLUVicent <17390955615@163.com>
2023-04-23 23:51:09 +0800

    2023-04-23

--- 1094.拼车/1094-拼车.cpp
+++ 1094.拼车/1094-拼车.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    // static const int n = 1001;
-    // int diff[n];
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int n = 1001;
-        vector<int> diff(n);
-        for( int i = 0; i < trips.size();i++){
-            diff[trips[i][1]] += trips[i][0];
-            diff[trips[i][2]] -= trips[i][0];
+        vector<int> diff(1001,0);
+        for(auto trip:trips){
+            diff[trip[1]] += trip[0];
+            diff[trip[2]] -= trip[0];
         }
-        for( int i = 0; i < n; i++){
-            if(i != 0) diff[i] += diff[i-1];
-            if( diff[i] > capacity) return false;
+        for( int i = 0; i < diff.size(); i++){
+            if( i != 0 ) diff[i] = diff[i]+diff[i-1];
+            if(diff[i] > capacity) return false;
         }
         return true;
 
