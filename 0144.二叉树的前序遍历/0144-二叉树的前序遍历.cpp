8904fbc184ef91a2ad87f830aae1b1c1168d4a71
JLUVicent <17390955615@163.com>
2023-04-27 23:51:12 +0800

    2023-04-27

--- 0144.二叉树的前序遍历/0144-二叉树的前序遍历.cpp
+++ 0144.二叉树的前序遍历/0144-二叉树的前序遍历.cpp
@@ -11,20 +11,37 @@
  */
 class Solution {
 public:
-    
-    void traversal(TreeNode* node , vector<int>& result){
-        if( node == NULL ) return;
-        
-        result.push_back( node->val );
-        traversal( node->left ,result);
-        traversal( node->right ,result);
-    }
-    
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> result;
-        traversal( root, result );
-        return result;
-        
+    //递归函数的参数和返回值
+    void traversal(TreeNode* node, vector<int>& vec){
+        //确定终止条件
+        if( node == NULL) return;
 
+        //确定单层递归逻辑
+        vec.push_back(node->val);//中
+        traversal(node->left,vec);   //左
+        traversal(node->right,vec); //右
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        // 使用栈来进行遍历
+        // stack<TreeNode*> st;
+        // //存储结果
+        // vector<int> result;
+        // if( root == NULL ) return result;
+        // st.push(root);
+        // // result.push_back(root->val);
+        // while( !st.empty() ){
+        //     TreeNode* node = st.top();
+        //     st.pop();
+        //     result.push_back(node->val);
+        //     if( node->right ) st.push(node->right);//入栈先入右孩子再入左孩子
+        //     if( node->left ) st.push(node->left);
+        // }
+        // return result;
+        
+        //递归遍历
+        vector<int> result;
+        traversal(root,result);
+        return result;
     }
 };
--- 0145.二叉树的后序遍历/0145-二叉树的后序遍历.cpp
+++ 0145.二叉树的后序遍历/0145-二叉树的后序遍历.cpp
@@ -12,19 +12,36 @@
 class Solution {
 public:
 
-    void traversal( TreeNode* node,vector<int>& vec){
-        if( node == NULL ){
-            return;
-        }
-        traversal( node->left,vec);
-        traversal( node->right,vec);
-        vec.push_back( node->val );
-    }
+    // void traversal(TreeNode* node,vector<int>& vec){
+    //     if( node == NULL) return;
+    //     traversal(node->left,vec);
+    //     traversal(node->right,vec);
+    //     vec.push_back(node->val);
+    // }
 
     vector<int> postorderTraversal(TreeNode* root) {
+        // vector<int> result;
+        // traversal(root,result);
+        // return result;
+
+        //使用栈
+        //使用中右左，最后再反转数组即可
+        //判断空数组
         vector<int> result;
-        traversal( root,result);
+        if( root == NULL) return result;
+        
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);    //中
+            if( node->left ) st.push(node->left);   //左
+            if( node->right ) st.push(node->right); //右
+        }
+        reverse(result.begin(),result.end());
         return result;
+
 
     }
 };
